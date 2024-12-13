30cb134e2a84c7afc7027c1edfc02865f9cda85f
Hemang-solanki <157679625+Hemang-solanki@users.noreply.github.com>
2024-12-13 13:07:36 +0530

    HEMANG-SOLANKI.GITHUB.IO

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -1,270 +1,303 @@
-import img1 from '../Images/amazon.png'
-import img2 from '../Images/nike.png'
+import img1 from "../Images/amazon.png";
+import img2 from "../Images/nike.png";
 export const skills = [
-    {
-        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
-        title: "HTML",
-        tag: "frontend"
-    },
-    {
-        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
-        title: "CSS",
-        tag: "frontend"
-    },
-    {
-        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-        title: "JavaScript",
-        tag: "frontend"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=XH6rVkDQCZ9U&format=png&color=000000",
-        title: "JQuery",
-        tag: "frontend"
-    },
-    {
-        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-        title: "Passport js",
-        tag: "backend"
-    },
-    {
-        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
-        title: "react",
-        tag: "frontend"
-    },
-    
-    {
-        icon: "https://img.icons8.com/color/512/chakra-ui.png",
-        title: "Chakra-UI",
-        tag: "frontend"
-    },
-    {
-        icon: "https://img.icons8.com/fluency/512/node-js.png",
-        title: "Node.js",
-        tag: "backend"
-    },
-    {
-        icon: "https://img.icons8.com/?size=512&id=gFw7X5Tbl3ss&format=png",
-        title: "Material-UI",
-        tag: "frontend"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=22448&format=png&color=000000",
-        title: "json",
-        tag: "frontend"
-    },
-    
-    {
-        icon: "https://img.icons8.com/?size=512&id=YO3YqSaTOu5K&format=png",
-        title: "vite",
-        tag: "frontend"
-    },
-    {
-        icon: "https://www.svgrepo.com/show/452093/redux.svg",
-        title: "redux",
-        tag: "frontend"
-    },
-    {
-        icon: "https://img.icons8.com/color/512/mongodb.png",
-        title: "MongoDB",
-        tag: "backend"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
-        title: "Tailwind",
-        tag: "frontend"
-    },
-    {
-        icon: "https://img.icons8.com/color/512/express-js.png",
-        title: "Express.js",
-        tag: "backend"
-    },
+  {
+    icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+    title: "HTML",
+    tag: "frontend",
+  },
+  {
+    icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+    title: "CSS",
+    tag: "frontend",
+  },
+  {
+    icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+    title: "JavaScript",
+    tag: "frontend",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=XH6rVkDQCZ9U&format=png&color=000000",
+    title: "JQuery",
+    tag: "frontend",
+  },
+  {
+    icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+    title: "Passport js",
+    tag: "backend",
+  },
+  {
+    icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+    title: "react",
+    tag: "frontend",
+  },
 
-    {
-        icon: "https://w7.pngwing.com/pngs/23/186/png-transparent-cloudflare-content-delivery-network-glassdoor-business-cloudbleed-others-service-orange-logo.png",
-        title: "Cloud Flare",
-        tag: "backend"
-    },
-    
-    {
-        icon: "https://img.icons8.com/?size=100&id=rHpveptSuwDz&format=png&color=000000",
-        title: "JWT",
-        tag: "backend"
-    },
-    {
-        icon: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png",
-        title: "Mongoose",
-        tag: "backend"
-    },
-    {
-        icon: "https://avatars.githubusercontent.com/u/67470890?s=200&v=4",
-        title: "Auth",
-        tag: "backend"
-    },
-    {
-        icon: "https://img.icons8.com/color/512/bootstrap.png",
-        title: "Bootstrap",
-        tag: "frontend"
-    },
+  {
+    icon: "https://img.icons8.com/color/512/chakra-ui.png",
+    title: "Chakra-UI",
+    tag: "frontend",
+  },
+  {
+    icon: "https://img.icons8.com/fluency/512/node-js.png",
+    title: "Node.js",
+    tag: "backend",
+  },
+  {
+    icon: "https://img.icons8.com/?size=512&id=gFw7X5Tbl3ss&format=png",
+    title: "Material-UI",
+    tag: "frontend",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=22448&format=png&color=000000",
+    title: "json",
+    tag: "frontend",
+  },
 
+  {
+    icon: "https://img.icons8.com/?size=512&id=YO3YqSaTOu5K&format=png",
+    title: "vite",
+    tag: "frontend",
+  },
+  {
+    icon: "https://www.svgrepo.com/show/452093/redux.svg",
+    title: "redux",
+    tag: "frontend",
+  },
+  {
+    icon: "https://img.icons8.com/color/512/mongodb.png",
+    title: "MongoDB",
+    tag: "backend",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
+    title: "Tailwind",
+    tag: "frontend",
+  },
+  {
+    icon: "https://img.icons8.com/color/512/express-js.png",
+    title: "Express.js",
+    tag: "backend",
+  },
 
-// Platforms & Tools
+  {
+    icon: "https://w7.pngwing.com/pngs/23/186/png-transparent-cloudflare-content-delivery-network-glassdoor-business-cloudbleed-others-service-orange-logo.png",
+    title: "Cloud Flare",
+    tag: "backend",
+  },
 
-    {
-        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
-        title: "Github",
-        tag: "platform"
-    },
-    {
-        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
-        title: "VS Code",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/color/512/git.png",
-        title: "Git",
-        tag: "platform"
-    },
-    {
-        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/New_Replit_Logo.svg/1200px-New_Replit_Logo.svg.png",
-        title: "Replit",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-codesandbox-an-online-code-editor-and-sharing-web-application-projects-logo-color-tal-revivo.png",
-        title: "Sandbox",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/color/512/firebase.png",
-        title: "Firebase",
-        tag: "backend"
-    },
-    {
-        icon: "https://img.icons8.com/ios-filled/512/codepen.png",
-        title: "CodePen",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=QEQQKirln6Tf&format=png&color=000000",
-        title: "Postman",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=ejub91zEY6Sl&format=png&color=000000",
-        title: "Chrome",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=6RHskkZGRABM&format=png&color=000000",
-        title: "Sublime",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=gXoJoyTtYXFg&format=png&color=000000",
-        title: "Windows",
-        tag: "platform"
-    },
-    {
-        icon: "https://img.icons8.com/?size=100&id=24895&format=png&color=000000",
-        title: "NPM",
-        tag: "platform"
-    },
-    {
-        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXGxwl9CXMlTQ3ngKLiw6DgkOb6pQlcjUwDQ&s",
-        title: "Vercel",
-        tag: "platform"
-    },
+  {
+    icon: "https://img.icons8.com/?size=100&id=rHpveptSuwDz&format=png&color=000000",
+    title: "JWT",
+    tag: "backend",
+  },
+  {
+    icon: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png",
+    title: "Mongoose",
+    tag: "backend",
+  },
+  {
+    icon: "https://avatars.githubusercontent.com/u/67470890?s=200&v=4",
+    title: "Auth",
+    tag: "backend",
+  },
+  {
+    icon: "https://img.icons8.com/color/512/bootstrap.png",
+    title: "Bootstrap",
+    tag: "frontend",
+  },
 
-]
+  // Platforms & Tools
+
+  {
+    icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+    title: "Github",
+    tag: "platform",
+  },
+  {
+    icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+    title: "VS Code",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/color/512/git.png",
+    title: "Git",
+    tag: "platform",
+  },
+  {
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/New_Replit_Logo.svg/1200px-New_Replit_Logo.svg.png",
+    title: "Replit",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-codesandbox-an-online-code-editor-and-sharing-web-application-projects-logo-color-tal-revivo.png",
+    title: "Sandbox",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/color/512/firebase.png",
+    title: "Firebase",
+    tag: "backend",
+  },
+  {
+    icon: "https://img.icons8.com/ios-filled/512/codepen.png",
+    title: "CodePen",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=QEQQKirln6Tf&format=png&color=000000",
+    title: "Postman",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=ejub91zEY6Sl&format=png&color=000000",
+    title: "Chrome",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=6RHskkZGRABM&format=png&color=000000",
+    title: "Sublime",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=gXoJoyTtYXFg&format=png&color=000000",
+    title: "Windows",
+    tag: "platform",
+  },
+  {
+    icon: "https://img.icons8.com/?size=100&id=24895&format=png&color=000000",
+    title: "NPM",
+    tag: "platform",
+  },
+  {
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXGxwl9CXMlTQ3ngKLiw6DgkOb6pQlcjUwDQ&s",
+    title: "Vercel",
+    tag: "platform",
+  },
+];
+
+// //////////////////////////////////////////////////
+
 export const projects = [
-    {
-        id: 0,
-        title: 'Amazone-Clone',
-        type: 'E-COMMERCE',
-        image: img1,
-        description: 'This Amazon clone effectively replicates essential e-commerce functionalities, offering a user-friendly, responsive interface for an engaging shopping experience.',
-        techStack: ['Java-Script'],
-        github: 'https://github.com/Hemang-solanki/amazon-clone-by-javascript',
-        liveDemo: 'https://amazon-clone-by-javascript.vercel.app/'
-    },
-    {
-        id: 1,
-        title: 'Nike-Clone',
-        type: 'E-COMMERCE',
-        image: img2,
-        description: "This Nike clone delivers a dynamic, responsive shopping experience, efficiently replicating key e-commerce functionalities for intuitive user interaction.",
-        techStack: ['React', 'Firebase', 'Tailwind',],
-        github: 'https://github.com/Hemang-solanki/nike',
-        liveDemo: 'https://stellular-kangaroo-4b1b67.netlify.app/'
-    },
-    {
-        id: 2,
-        title: "ForeignBazar",
-        type: "E-COMMERCE",
-        image: "https://user-images.githubusercontent.com/94675329/212765286-55e5183b-598b-4648-96b6-1417b234b1b9.png",
-        description: "ForeignBazar is an e-commerce website, where you can take a look on all daily needs like Clothes, Cosmetics etc. And you can go for order them. It's have the admin functionality.",
-        techStack: ["React", "CSS", "ReactJS", "Chakra-UI"],
-        github: "https://github.com/Atanu8250/ForeignBazar",
-        liveDemo: "https://foreign-bazar.netlify.app/"
-    },
-    {
-        id: 3,
-        title: 'First-AIM',
-        type: 'Multi-Player Game',
-        image: 'https://github.com/Atanu8250/fast-aim/blob/main/frontend/src/Asset/Images/HomePage.png?raw=true',
-        description: 'Fast Aim is a gaming Web-application that allows playing both Single Player and Multi-Player. Try to beat your opponent',
-        techStack: ['React', 'Chakra-UI', 'TypeScript', 'Express.js', 'Web-Socket', 'Node.js'],
-        github: 'https://github.com/Atanu8250/fast-aim',
-        liveDemo: 'https://fast-aim-game.vercel.app/'
-    },
-    {
-        id: 4,
-        title: "GadgetRambo",
-        type: "E-COMMERCE",
-        image: "https://user-images.githubusercontent.com/94675329/215067790-242b6566-714a-4eaf-b992-63b466f04c08.png",
-        description: "GadgetRambo is a technology review, news and e-commerce platform. Here we provided full e-commerce flow and also the admin part.",
-        techStack: ["Typescript", "Next.js", "Redux", "CSS", "Firebase", "thunk", "chakra-ui", "MUI"],
-        github: "https://github.com/Atanu8250/GadgetRambo",
-        liveDemo: "https://gadget-rambo.vercel.app/"
-    },
-    {
-        id: 5,
-        title: "YouTube clone",
-        type: "OTT PLATFORM",
-        image: "https://user-images.githubusercontent.com/94675329/212769255-f619312f-a91f-42cf-8f49-cfde9ef8d282.PNG",
-        description: "In this YouTube Clone You can search for any Videos and at the time of watching a video you will get Recommendation also. You can Do Filter by View, Alphabet, Popularity.",
-        techStack: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/Atanu8250/YouTube-clone",
-        liveDemo: "https://youtube-cloned-by-atanu.netlify.app/"
-    },
-    {
-        id: 6,
-        title: "Neifty PM",
-        type: "PROJECT MANAGER",
-        image: "https://user-images.githubusercontent.com/94675329/212766385-81cad39c-a83e-4a08-87de-5a539e12ef8d.png",
-        description: "Nifty, it's a remote collaboration hub to manage Projects, tasks, communication all in one place. Here you can collaborate with your team within an easy way.",
-        techStack: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/Atanu8250/cared-eggs-1276",
-        liveDemo: "https://nifty-cloned-by-atanu.netlify.app/"
-    },
-    {
-        id: 7,
-        title: "Health-Dr.",
-        type: "DIET MENTOR",
-        image: "https://user-images.githubusercontent.com/94675329/212766892-aa364775-05a3-49c1-acf9-5fb07bb1c5b9.png",
-        description: "Health-Dr. website is used to track your health-diet. Here you can set the goal to have a fitness full body and our website will track your fitness and health",
-        techStack: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/Atanu8250/Health-Dr.",
-        liveDemo: "https://health-dr-by-team-masai-warriors.netlify.app/"
-    },
-    {
-        id: 8,
-        title: "Pablo Parking",
-        type: "PARKING WEB APP",
-        image: "https://user-images.githubusercontent.com/94675329/212767301-b159c959-43fe-4c9c-8655-93d021829ab5.png",
-        description: "This project based on vehicle parking system created based on LLD (Low Level Designing) concept. Here you can book a slot to park your car.",
-        techStack: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/Vishwa9011/PabloParking",
-        liveDemo: "https://pabloparking.netlify.app/"
-    }
-]
+  {
+    id: 0,
+    title: "Amazone-Clone",
+    type: "E-COMMERCE",
+    image: img1,
+    description:
+      "This Amazon clone effectively replicates essential e-commerce functionalities, offering a user-friendly, responsive interface for an engaging shopping experience.",
+    techStack: ["Java-Script" , "HTML" , "CSS" , "React" ],
+    github: "https://github.com/Hemang-solanki/amazon-clone-by-javascript",
+    liveDemo: "https://amazon-clone-by-javascript.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Nike-Clone",
+    type: "E-COMMERCE",
+    image: img2,
+    description:
+      "This Nike clone delivers a dynamic, responsive shopping experience, efficiently replicating key e-commerce functionalities for intuitive user interaction.",
+    techStack: ["React", "Firebase", "CSS" , "JavaScript" , "HTML"],
+    github: "https://github.com/Hemang-solanki/nike",
+    liveDemo: "https://stellular-kangaroo-4b1b67.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "ForeignBazar",
+    type: "E-COMMERCE",
+    image:
+      "https://user-images.githubusercontent.com/94675329/212765286-55e5183b-598b-4648-96b6-1417b234b1b9.png",
+    description:
+      "ForeignBazar is an e-commerce website, where you can take a look on all daily needs like Clothes, Cosmetics etc. And you can go for order them. It's have the admin functionality.",
+    techStack: ["React", "CSS", "ReactJS", "Chakra-UI"],
+    github: "",
+    liveDemo: "https://foreign-bazar.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "First-AIM",
+    type: "Multi-Player Game",
+    image:
+      "https://github.com/Atanu8250/fast-aim/blob/main/frontend/src/Asset/Images/HomePage.png?raw=true",
+    description:
+      "Fast Aim is a gaming Web-application that allows playing both Single Player and Multi-Player. Try to beat your opponent",
+    techStack: [
+      "React",
+      "Chakra-UI",
+      "TypeScript",
+      "Express.js",
+      "Web-Socket",
+      "Node.js",
+    ],
+    github: "https://github.com/Hemang-solanki/fast-aim",
+    liveDemo: "https://fast-aim-game.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "GadgetRambo",
+    type: "E-COMMERCE",
+    image:
+      "https://user-images.githubusercontent.com/94675329/215067790-242b6566-714a-4eaf-b992-63b466f04c08.png",
+    description:
+      "GadgetRambo is a technology review, news and e-commerce platform. Here we provided full e-commerce flow and also the admin part.",
+    techStack: [
+      "Typescript",
+      "Next.js",
+      "Redux",
+      "CSS",
+      "Firebase",
+      "thunk",
+      "chakra-ui",
+      "MUI",
+    ],
+    github: "https://github.com/Hemang-solanki/GadgetRambo-master",
+    liveDemo: "https://gadget-rambo.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "YouTube clone",
+    type: "OTT PLATFORM",
+    image:
+      "https://user-images.githubusercontent.com/94675329/212769255-f619312f-a91f-42cf-8f49-cfde9ef8d282.PNG",
+    description:
+      "In this YouTube Clone You can search for any Videos and at the time of watching a video you will get Recommendation also. You can Do Filter by View, Alphabet, Popularity.",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Hemang-solanki/YouTube-clone-master",
+    liveDemo: "https://youtube-cloned-by-atanu.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Neifty PM",
+    type: "PROJECT MANAGER",
+    image:
+      "https://user-images.githubusercontent.com/94675329/212766385-81cad39c-a83e-4a08-87de-5a539e12ef8d.png",
+    description:
+      "Nifty, it's a remote collaboration hub to manage Projects, tasks, communication all in one place. Here you can collaborate with your team within an easy way.",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Hemang-solanki/Nifty-Pm-master",
+    liveDemo: "https://nifty-cloned-by-atanu.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Health-Dr.",
+    type: "DIET MENTOR",
+    image:
+      "https://user-images.githubusercontent.com/94675329/212766892-aa364775-05a3-49c1-acf9-5fb07bb1c5b9.png",
+    description:
+      "Health-Dr. website is used to track your health-diet. Here you can set the goal to have a fitness full body and our website will track your fitness and health",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Hemang-solanki/Health-Dr.-master",
+    liveDemo: "https://health-dr-by-team-masai-warriors.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Pablo Parking",
+    type: "PARKING WEB APP",
+    image:
+      "https://user-images.githubusercontent.com/94675329/212767301-b159c959-43fe-4c9c-8655-93d021829ab5.png",
+    description:
+      "This project based on vehicle parking system created based on LLD (Low Level Designing) concept. Here you can book a slot to park your car.",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Hemang-solanki/PabloParking-main",
+    liveDemo: "https://pabloparking.netlify.app/",
+  },
+];
